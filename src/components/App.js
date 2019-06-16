a42808bec231cb2c70fb3ad399e68f1c2ddaa19c
Mang506 <mrubly@live.com>
2019-06-16 08:51:09 -0700

    API feature added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
-
 class App extends React.Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: [] 
   };
   componentDidMount(){
     // timers, liseners... ajax etc
+    axios.get('/api/contests')
+      .then(resp => {
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error);
+
+    // this.setState({
+    //   contests: data.contests
+    // });
   }
   componentWillMount(){
     // clean timers, listeners... removes gunk from didMount
@@ -16,11 +28,12 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          {this.props.contests.map(contest => 
-            <ContestPreview key={contest.id} {...contest} />
+          {this.state.contests.map(contest => 
+            // each element needs unique id, dont use array index id
+            <ContestPreview key={contest.id} {...contest} /> 
           )}
         </div>
       </div>
