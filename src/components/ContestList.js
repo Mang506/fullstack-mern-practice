6bf396665ba40baafa5e1e6f4fd7af94b65757f1
Mang506 <mrubly@live.com>
2019-06-18 19:14:52 -0700

    Added URL history functionality

--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -2,23 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ContestPreview from './ContestPreview';
 
-const ContestList = ({ contests }) => (
+const ContestList = ({ contests, onContestClick }) => (
   <div className="ContestList">
     {contests.map(contest =>
-      <ContestPreview key={contest.id} {...contest} />  
+      <ContestPreview 
+        key={contest.id}
+        onClick={onContestClick}
+        {...contest} />  
     )}
 
   </div>
 );
 
 ContestList.protoTypes = {
-  contests: PropTypes.array
+  contests: PropTypes.array,
+  onContestClick: PropTypes.func.isRequired,
 };
 
 export default ContestList;
-
-{/* <div>
-          {this.state.contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest} />
-          )}
-        </div> */}
